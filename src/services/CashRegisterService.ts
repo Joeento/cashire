dcf3358ffaf6eda322d9c9a5743429cd59c9b54f
Eric Kudler <eric.kudler@hearst.com>
2021-03-10 01:28:45 -0500

    Adds a basic function to our cash register

---
+++ src/services/CashRegisterService.ts
@@ -0,0 +1,11 @@
+import { ProductInterface } from "../interfaces/ProductInterface";
+
+export class CashRegisterService {
+  static tallyPrice(products:ProductInterface[]): number {
+    let sum = 0;
+    for (let product of products) {
+      sum += product.price;
+    }
+    return sum;
+  }
+}
