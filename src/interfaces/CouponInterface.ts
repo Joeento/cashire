2f276a6afd7ec99d104106d874dc034d717df5ea
Eric Kudler <eric.kudler@hearst.com>
2021-03-11 00:24:29 -0500

    calculateDiscount should be in every coupon

--- src/interfaces/CouponInterface.ts
+++ src/interfaces/CouponInterface.ts
@@ -1,4 +1,7 @@
+import { ProductInterface } from "./ProductInterface";
+
 export interface CouponInterface {
   id: number;
   name: string;
+  calculateDiscount(product: ProductInterface): number;
 }
