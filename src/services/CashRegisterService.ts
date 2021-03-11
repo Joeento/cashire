6b18deea73e89cab80ba16b04290cb2689c824a3
Eric Kudler <eric.kudler@hearst.com>
2021-03-11 16:45:38 -0500

    This service is no longer static since it needs a tax service injected

--- src/services/CashRegisterService.ts
+++ src/services/CashRegisterService.ts
@@ -1,15 +1,20 @@
 import { CouponInterface } from "../interfaces/CouponInterface";
 import { ProductInterface } from "../interfaces/ProductInterface";
+import { TaxService } from "../services/TaxService";
 
 export class CashRegisterService {
-  static tallyPrice(products:ProductInterface[]): number {
+  taxService: TaxService;
+  constructor(taxService: TaxService) {
+    this.taxService = taxService;
+  }
+  tallyPrice(products:ProductInterface[]): number {
     let sum = 0;
     for (let product of products) {
       sum += product.price();
     }
     return Math.max(sum, 0.00);
   }
-  static applyDiscount(product: ProductInterface, coupon: CouponInterface): number {
+  applyDiscount(product: ProductInterface, coupon: CouponInterface): number {
     const discount = coupon.calculateDiscount(product);
     return product.price() - discount;
   }
--- tests/cash_register_tests.spec.ts
+++ tests/cash_register_tests.spec.ts
@@ -6,24 +6,27 @@ import { ProductInterface } from '../src/interfaces/ProductInterface';
 import { Cereal } from '../src/products/Cereal';
 import { Fruit } from '../src/products/Fruit';
 import { CashRegisterService } from '../src/services/CashRegisterService';
+import { TaxService } from '../src/services/TaxService';
 
 describe('Cash Register',
   () => {
+    const taxService: TaxService = new TaxService();
+    const cashRegisterService: CashRegisterService = new CashRegisterService(taxService);
     it('should take off percentage discount', () => {
       const cheerios: Cereal = new Cereal(1, "Cheerios", 2.00);
       const tenPercentCoupon: PercentDiscountCoupon = new PercentDiscountCoupon(1, "10% Off Produce!", 10);
-      expect(CashRegisterService.applyDiscount(cheerios, tenPercentCoupon)).to.equal(1.80);
+      expect(cashRegisterService.applyDiscount(cheerios, tenPercentCoupon)).to.equal(1.80);
     });
     it('should take off cash discount', () => {
       const cheerios: Cereal = new Cereal(1, "Cheerios", 3.00);
       const oneDollarCoupon: PriceDiscountCoupon = new PriceDiscountCoupon(1, "$1.00 Off Produce!", 1);
-      expect(CashRegisterService.applyDiscount(cheerios, oneDollarCoupon)).to.equal(2.00);
+      expect(cashRegisterService.applyDiscount(cheerios, oneDollarCoupon)).to.equal(2.00);
     });
     it('should correctly sum costs', () => {
       const apple: Fruit = new Fruit(1, "Apple", 5.00, 1);
       const orange: Fruit = new Fruit(2, "orange", 3.00, 1);
       const cheerios: Cereal = new Cereal(3, "Cheerios", 2.00);
       const products: ProductInterface[] = [apple, orange, cheerios];
-      expect(CashRegisterService.tallyPrice(products)).to.equal(10.00);
-  });
+      expect(cashRegisterService.tallyPrice(products)).to.equal(10.00);
+    });
 });
