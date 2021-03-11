7ba744adfff937b9e17d422cfad91b7ccdebb4a3
Eric Kudler <eric.kudler@hearst.com>
2021-03-11 00:57:18 -0500

    Updated list of classes to compile and export

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 import { CouponInterface } from "./src/interfaces/CouponInterface";
 import { ProductInterface } from "./src/interfaces/ProductInterface";
 
+import { PercentDiscountCoupon } from "./src/coupons/PercentDiscountCoupon";
+import { PriceDiscountCoupon } from "./src/coupons/PriceDiscountCoupon";
 
-export { CouponInterface, ProductInterface }
+import { Cereal } from "./src/products/Cereal";
+import { Fruit } from "./src/products/Fruit";
+
+import { CashRegisterService } from "./src/services/CashRegisterService";
+
+export { CouponInterface,
+         ProductInterface,
+         PercentDiscountCoupon,
+         PriceDiscountCoupon,
+         Cereal,
+         Fruit,
+         CashRegisterService
+        }
